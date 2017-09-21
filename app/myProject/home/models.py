06f46b55e856f9386f5f2fb711d455c917272f8e
omegabiscuit <bb3uc@virginia.edu>
2017-09-21 18:08:23 -0400

    added models

--- app/myProject/home/models.py
+++ app/myProject/home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
+
 # Create your models here.
+class Location(models.Model):
+    building_name = models.CharField(max_length = 50)
+    building_address = models.CharField(max_length = 50)
+    College_name = models.CharField(max_length = 50)
+    
+class Students(models.Model):
+    name = models.CharField()
+    year = models.IntegerField()
+
+class Groups(models.Model):
+    size = models.IntegerField()
+    
