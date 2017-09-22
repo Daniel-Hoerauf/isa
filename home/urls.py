81855e06561af47eaa962625094477c623d6fc15
omegabiscuit <bb3uc@virginia.edu>
2017-09-21 21:13:28 -0400

    added code from student branch's views.py and urls.py. Also, changed location info

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,24 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     
+    
+    #location get requests
+    url(r'^location/all/$', views.location_index, name='location_index'),
+    url(r'^location/new/$', views.add_location, name='add_location'),
+    url(r'^location/(?P<location>[0-9]+)/$', views.get_location,
+        name='get_location'),
+    url(r'^location/(?P<location>[0-9]+)/delete/$', views.delete_location,
+        name='delete_location'),
+    url(r'^location/(?P<location>[0-9]+)/update/$', views.update_location,
+        name='update_location'),
+    
+    #Student get requests
+    url(r'^student/all/$', views.student_index, name='student_index'),
+    url(r'^student/new/$', views.create_student, name='create_student'),
+    url(r'^student/(?P<student>[0-9]+)/$', views.get_student,
+        name='get_student'),
+    url(r'^student/(?P<student>[0-9]+)/delete/$', views.delete_student,
+        name='delete_student'),
+    url(r'^student/(?P<student>[0-9]+)/update/$', views.update_student,
+        name='update_student'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,23 +1,54 @@
-from django.shortcuts import render, HttpResponse
-from django.db import models
-from django.views import generic
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.shortcuts import HttpResponse
 from django.http import JsonResponse
-from .models import Location
+from .models import Location, Student
 
 
 def index(request):
     return HttpResponse("<h1>Hello, world.</h1>")
 
-class LocationView(generic.DetailView):
-    def get_queryset(self):
-        model = Location
-        template_name = 'home/location.html'
-        loc_index_dict = {}
-        for loc in Location.objects.all():
-            print(loc.building_name)
-            loc_index_dict["building name"] = loc.building_name
-            loc_index_dict["college name"] = loc.college_name
-            loc_index_dict["building address"] = loc.building_address
-        return JsonResponse(loc_index_dict)
+def location_index(self):
+    model = Location
+    resp = {'status': 'empty', 'locations': []}
+    for loc in Location.objects.all():
+        resp['locations'].append({
+            'building_name':loc.building_name,
+            'building_address': loc.building_address,
+            'college_name': loc.college_name,
+            })
+    return JsonResponse(resp)
 
+def add_location(request):
+    return HttpResponse("Add Location")
+
+def get_location(request, location):
+    return HttpResponse("Get Location #{{".format(location))
+                                         
+def delete_location(request, location):
+    return HttpResponse("Delete Location #{}".format(location))
+
+def update_location(request, location):
+    return HttpResponse("Delete Location #{}".format(location))
+
+def student_index(request):
+    resp = {'status': 'ok', 'students': []}
+    for stud in Student.objects.all():
+        resp['students'].append({
+            'name': stud.name,
+            'year': stud.year,
+            'groups': [{'id': gr.pk,
+                        'name': gr.name}
+                       for gr in stud.group_set.all()],
+        })
+    return JsonResponse(resp)
+
+def create_student(request):
+    return HttpResponse("Create Student")
+
+def get_student(request, student):
+    return HttpResponse("Get Student #{}".format(student))
+
+def delete_student(request, student):
+    return HttpResponse("Delete Student #{}".format(student))
+
+def update_student(request, student):
+    return HttpResponse("Update Student #{}".format(student))
