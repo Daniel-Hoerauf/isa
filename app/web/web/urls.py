2e69fad7bc961389315cf765b2e73e4de0c310bb
Brigham Bangerter <bb3uc@virginia.edu>
2017-10-04 11:48:34 -0400

    minor changes to urls

--- app/web/web/urls.py
+++ app/web/web/urls.py
@@ -17,5 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', views.hello),
     url(r'^admin/', admin.site.urls),
+    url(r'^group/', views.group),
 ]
