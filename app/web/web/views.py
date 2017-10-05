03914056d95554f2a9b02b92b11c908666ab6126
omegabiscuit <bb3uc@virginia.edu>
2017-10-05 19:57:54 -0400

    minor adjustments to main page

--- app/web/web/views.py
+++ app/web/web/views.py
@@ -14,11 +14,13 @@ import json
 
 
 def hello(request):
-    return render(request, 'mainPage/mainPage.html')
+    
     #template_name = 'templates/mainPage/mainPage.html'
     req = urllib.request.Request('http://exp-api:8000/group/all')
     resp_json = urllib.request.urlopen(req).read().decode('utf-8')
     resp = json.loads(resp_json)
+    
+    return render(request, 'mainPage/mainPage.html')
     return JsonResponse(resp)
     #return render(request, 'app/mainPage.html')
     return HttpResponse('Hello main page\n')
@@ -27,7 +29,7 @@ def groupDetail(request):
      req = urllib.request.Request('http://exp-api:8000/group/1')
      resp_json = urllib.request.urlopen(req).read().decode('utf-8')
      resp = json.loads(resp_json)
-     print(resp)
-     return HttpResponse('Hello group page\n')
+     #print(resp)
+     #return HttpResponse('Hello group page\n')
      return JsonResponse(resp)
      return HttpResponse('Hello group page\n')
