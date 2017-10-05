3aaa9d2588cea4f81e615e73ab1619539c9de4a2
Brigham Bangerter <bb3uc@virginia.edu>
2017-10-04 20:27:58 -0400

    added exp view

--- app/exp/exp/views.py
+++ app/exp/exp/views.py
@@ -5,3 +5,12 @@ def hello(request):
 
 def group(request):
     return HttpResponse('api group\n')
+
+class CreateView(generics.ListCreateAPIView):
+    """This class defines the create behavior of our rest api."""
+    queryset = Bucketlist.objects.all()
+    serializer_class = BucketlistSerializer
+
+    def perform_create(self, serializer):
+        """Save the post data when creating a new bucketlist."""
+        serializer.save()
