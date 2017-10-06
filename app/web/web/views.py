3b9adc33c6b75bb23a5b27394006809cb3b28dc7
omegabiscuit <bb3uc@virginia.edu>
2017-10-05 20:44:28 -0400

    api info now shows in templates

--- app/web/web/views.py
+++ app/web/web/views.py
@@ -19,17 +19,24 @@ def hello(request):
     req = urllib.request.Request('http://exp-api:8000/group/all')
     resp_json = urllib.request.urlopen(req).read().decode('utf-8')
     resp = json.loads(resp_json)
+    groupList = resp['groups']
     
-    return render(request, 'mainPage/mainPage.html')
+    #return HttpResponse(groupList)
+    return render(request, 'mainPage/mainPage.html', {'groupList':groupList})
     return JsonResponse(resp)
     #return render(request, 'app/mainPage.html')
-    return HttpResponse('Hello main page\n')
+    
 
 def groupDetail(request):
      req = urllib.request.Request('http://exp-api:8000/group/1')
      resp_json = urllib.request.urlopen(req).read().decode('utf-8')
      resp = json.loads(resp_json)
+     group = resp['group']
+     size = group['size']
+     name = group['name']
      #print(resp)
      #return HttpResponse('Hello group page\n')
+     #return HttpResponse(size)
+     return render(request, 'mainPage/group.html', {'name': name, 'size':size})
      return JsonResponse(resp)
-     return HttpResponse('Hello group page\n')
+     
