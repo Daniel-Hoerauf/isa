facd1108b5dc87fe8e9bb164cf9fe2d6f985ed6c
omegabiscuit <bb3uc@virginia.edu>
2017-10-28 18:35:47 -0400

    added login/logout views

--- app/exp/exp/views.py
+++ app/exp/exp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
 import urllib.request
 import urllib.parse
+from django.core.urlresolvers import reverse
 import json
 import requests
 from django.http import JsonResponse
@@ -28,19 +29,27 @@ def get_group(request, group):
     return JsonResponse(resp)
 
 def create_user(request):
+    #data dict is only to be used until web layer is complete
     data = {}
     data['username'] = 'tester'
     data['name'] = 'brigham'
     data['password'] = 'notpassword'
     data['year'] = 4
-    r = requests.post('http://models-api:8000/signup/',data).json()
-    if r['status'] == 'ok':
-        return HttpResponse(r['status'])
-    else:
-        return HttpResponse(r['status'])
+    resp = requests.post('http://models-api:8000/signup/',data).json()
+    return JsonResponse(resp)
 
 def login(request):
-    return JsonResponse({'status': 'ok'})
+    resp = requests.post(reverse('login', args=[user.pk]),{'password': 'p4ssw0rd'}).json()
+    if resp['status']=='ok' and resp['authenticated']==True:
+        auth = Authenticator.objects.get(pk=resp['authenticator'])
+        if auth.user_id == user:
+            return True
+    return False
+    #return JsonResponse(resp)
+    #return JsonResponse({'status': 'ok'})
 
 def logout(request):
-    return JsonResponse({'status': 'ok'})
+    auth = create_authenticator(user)
+    if Authenticator.objects.get(authenticator=auth) == True:
+        resp = requests.post(reverse('logout'), {'authenticator': auth}).json()
+    return JsonResponse(resp)
