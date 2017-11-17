4dde85d61d4d5f30cb222a6c25e81ec2167ccdda
David <ddh3jw@virginia.edu>
2017-11-16 23:43:48 -0500

    Updated Integration Tests

--- app/web/web/testSearchPage.py
+++ app/web/web/testSearchPage.py
@@ -7,26 +7,6 @@ from selenium.webdriver.support import expected_conditions as EC # available sin
 WebDriver driver = new ChromeDriver()
 
 # go to the google home page
-driver.get("http://localhost:8000")
+driver.get("http://localhost:8000/search/")
 
-# the page is ajaxy so the title is originally this:
 print driver.title
-
-# find the element that's name attribute is q (the google search box)
-inputElement = driver.find_element_by_name("q")
-
-# type in the search
-inputElement.send_keys("cheese!")
-
-# submit the form (although google automatically searches now without submitting)
-inputElement.submit()
-
-try:
-    # we have to wait for the page to refresh, the last thing that seems to be updated is the title
-    WebDriverWait(driver, 10).until(EC.title_contains("cheese!"))
-
-    # You should see "cheese! - Google Search"
-    print driver.title
-
-finally:
-    driver.quit()
